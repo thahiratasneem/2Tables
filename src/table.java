873891d7eac5b17d9836314e66de981b51687983
thahiratasneem <sheik@Sheiks-MacBook-Pro.local>
2014-08-19 15:53:33 +0530

    2Table

---
+++ src/table.java
@@ -0,0 +1,15 @@
+
+public class table {
+
+	public static void main(String args[])
+	{
+		int n=2;
+		int i=1;
+		while( i<=5)
+		{
+			System.out.println(n + "*" + i + "=" +(n*i));
+i++;
+		}
+	}
+
+}
